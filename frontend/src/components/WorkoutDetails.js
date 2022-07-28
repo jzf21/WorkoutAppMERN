463b3339d49882d06b207be9d63bf55e05ff07e9
jzf21 <jozefneelamkavil2003@gmail.com>
2022-07-28 11:27:38 +0530

    made it pink

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -16,9 +16,10 @@ export const WorkoutDetails = ({workout}) => {
   return (
     <div className='workout-details'>
         <h4>{workout.title}</h4>
-        <p><strong>Load(kg):</strong>{workout.load}</p>
-        <p><strong>Reps:</strong>{workout.reps}</p>
+        <p><strong>Load(kg): </strong>{workout.load}</p>
+        <p><strong>Reps: </strong>{workout.reps}</p>
         <p className='datecreated'>{formatDistanceToNow(new Date(workout.createdAt),{addSuffix:true})}</p>
+        
         <span onClick={handleClick}>Delete</span>
     </div>
   )
